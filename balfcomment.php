2cc1a3383306759ea32d3180d19d1eddf8f1d0b6
Jamie Balfour <jamie@balfour.scot>
2025-10-12 08:23:05 +0100

    Update balfcomment.php
    Added support for storing a user ID. This allows for checking if the admin or a user posted a comment or reply.

--- balfcomment.php
+++ balfcomment.php
@@ -5,11 +5,11 @@ class BalfComment {
   private $connection = null;
 
   public function createTables(){
-    $query1 = 'CREATE TABLE `BalfComment_Posts` (`comment_id` int(11) NOT NULL, `comment_url` varchar(100) NOT NULL, `comment_title` varchar(50) NOT NULL, `comment_text` text NOT NULL, `poster_name` varchar(20) NOT NULL, `timestamp` int(11) NOT NULL, `parent` int(11) NOT NULL DEFAULT -1)';
+    $query1 = 'CREATE TABLE `BalfComment_Posts` (`comment_id` int(11) NOT NULL, `comment_url` varchar(100) NOT NULL, `comment_title` varchar(50) NOT NULL, `comment_text` text NOT NULL, `poster_name` varchar(20) NOT NULL, `timestamp` int(11) NOT NULL, `parent` int(11) NOT NULL DEFAULT -1, `user_id` int(11) NOT NULL DEFAULT -1)';
 
-    $query2 = 'ALTER TABLE `BalfComment_Posts` ADD PRIMARY KEY (`comment_id`);'
+    $query2 = 'ALTER TABLE `BalfComment_Posts` ADD PRIMARY KEY (`comment_id`);';
 
-    $query3 = 'ALTER TABLE `BalfComment_Posts` MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT;'
+    $query3 = 'ALTER TABLE `BalfComment_Posts` MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT;';
   }
 
   public function __construct($con){
@@ -38,12 +38,12 @@ class BalfComment {
 
   }
 
-  public function newComment($url, $title, $text, $poster_name, $parent = -1){
+  public function newComment($url, $title, $text, $poster_name, $parent = -1, $user_id = -1){
     $timestamp = time();
-    $query = "INSERT INTO `BalfComment_Posts` (comment_url, comment_title, comment_text, poster_name, timestamp, parent) VALUES (:url, :title, :text, :poster, :timestamp, :parent)";
+    $query = "INSERT INTO `BalfComment_Posts` (comment_url, comment_title, comment_text, poster_name, timestamp, parent, user_id) VALUES (:url, :title, :text, :poster, :timestamp, :parent, :user_id)";
     $stmt = $this->connection->prepare($query);
 
-    if(!$stmt->execute(array(":url" => $url, ":title" => htmlentities($title), ":text" => htmlentities($text), ":poster" => htmlentities($poster_name), ":timestamp" => $timestamp, ":parent" => $parent))){
+    if(!$stmt->execute(array(":url" => $url, ":title" => htmlentities($title), ":text" => htmlentities($text), ":poster" => htmlentities($poster_name), ":timestamp" => $timestamp, ":parent" => $parent, ":user_id" => $user_id))){
       return false;
     }
 
@@ -66,16 +66,27 @@ class BalfComment {
         if($comment['parent'] != -1){
           $reply = true;
           $reply_class = " reply";
+
+          if($comment['user_id'] != -1){
+            $reply_class = " reply is_user";
+          }
         }
         $output .= '<div class="balfcomment'.$reply_class.'" id="balfcomment_id_'.$comment['comment_id'].'">';
           $output .= '<div class="wrapper">';
             $output .= '<div class="row">';
               $said = " said";
               if($reply){
-                $said = ' replied to <a href="#balf_comment_id_'.$comment['parent'].'">' . $comments[$comment['parent']]['poster_name'] . '</a>';
+                $said = ' replied to <a href="#balfcomment_id_'.$comment['parent'].'">' . $comments[$comment['parent']]['poster_name'] . '</a>';
+              }
+              $user = "";
+              if($comment['user_id'] != -1){
+                $user = '<span class="user">User</span>';
+                if($comment['user_id'] == 1){
+                  $user = '<span class="user">Site Admin</span>';
+                }
               }
               $output .= '<div class="col_lg_9 col_md_9 col_sm_8 col_xs_6 col_xxs_6">';
-                $output .= '<div class="poster">'.$comment['poster_name']. $said.'</div>';
+                $output .= '<div class="poster">'.$comment['poster_name'].$user. $said.'</div>';
               $output .= '</div>';
               $output .= '<div class="col_lg_3 col_md_3 col_sm_4 col_xs_6 col_xxs_6">';
                 $output .= '<div class="timestamp">'.date("Y-m-d H:i:s", $comment['timestamp']).'</div>';
