e36d8719ddac9c422d952d614c42373f9f547ba8
Jamie Balfour <jamie@balfour.scot>
2025-12-30 00:59:45 +0000

    Update BalfComment_Posts schema and queries
    Added the state value and IP address value to the table. IP is, of course, optional, but it can help with concerns around comments. I personally use it on my website and delete them after 1 week.

--- balfcomment.php
+++ balfcomment.php
@@ -5,7 +5,7 @@ class BalfComment {
   private $connection = null;
 
   public function createTables(){
-    $query1 = 'CREATE TABLE `BalfComment_Posts` (`comment_id` int(11) NOT NULL, `comment_url` varchar(100) NOT NULL, `comment_title` varchar(50) NOT NULL, `comment_text` text NOT NULL, `poster_name` varchar(20) NOT NULL, `timestamp` int(11) NOT NULL, `parent` int(11) NOT NULL DEFAULT -1, `user_id` int(11) NOT NULL DEFAULT -1)';
+    $query1 = 'CREATE TABLE `BalfComment_Posts` (`comment_id` int(11) NOT NULL, `comment_url` varchar(100) NOT NULL, `comment_title` varchar(50) NOT NULL, `comment_text` text NOT NULL, `poster_name` varchar(20) NOT NULL, `timestamp` int(11) NOT NULL, `state` int(11) NOT NULL DEFAULT 0, `parent` int(11) NOT NULL DEFAULT -1, `root_comment` int(11) NOT NULL DEFAULT -1, `user_id` int(11) NOT NULL DEFAULT -1)';
 
     $query2 = 'ALTER TABLE `BalfComment_Posts` ADD PRIMARY KEY (`comment_id`);';
 
@@ -18,7 +18,7 @@ class BalfComment {
 
   public function getComments($url){
     //Find parent comments first
-    $query = "SELECT * FROM `BalfComment_Posts` WHERE comment_url = :url AND parent = -1 ORDER BY timestamp";
+    $query = "SELECT * FROM `BalfComment_Posts` WHERE comment_url = :url AND parent = -1 AND state = 1 ORDER BY timestamp";
     $stmt = $this->connection->prepare($query);
 
     if(!$stmt->execute(array(":url" => $url))){
@@ -26,7 +26,7 @@ class BalfComment {
     }
 
     //Now find replies
-    $query = "SELECT * FROM `BalfComment_Posts` WHERE comment_url = :url AND parent > -1 ORDER BY timestamp";
+    $query = "SELECT * FROM `BalfComment_Posts` WHERE comment_url = :url AND parent > -1 AND state = 1 ORDER BY timestamp";
     $reply_statement = $this->connection->prepare($query);
 
     //Iterate the comments
@@ -44,19 +44,20 @@ class BalfComment {
 
     //Store the replies in the replies array
     while($comment = $reply_statement->fetch(PDO::FETCH_ASSOC)){
-      array_push($main_array[$comment['parent']]['replies'], $comment);
+      array_push($main_array[$comment['root_comment']]['replies'], $comment);
     }
 
     return $main_array;
 
   }
 
-  public function newComment($url, $title, $text, $poster_name, $parent = -1, $user_id = -1){
+  public function newComment($url, $title, $text, $poster_name, $state = 0, $parent = -1, $root = -1, $user_id = -1){
     $timestamp = time();
-    $query = "INSERT INTO `BalfComment_Posts` (comment_url, comment_title, comment_text, poster_name, timestamp, parent, user_id) VALUES (:url, :title, :text, :poster, :timestamp, :parent, :user_id)";
+    $ip_address = $_SERVER['REMOTE_ADDR'];
+    $query = "INSERT INTO `BalfComment_Posts` (comment_url, comment_title, comment_text, poster_name, timestamp, ip_address, parent, root_comment, user_id, state) VALUES (:url, :title, :text, :poster, :timestamp, :ip_addressm :parent, :root, :user_id, :state)";
     $stmt = $this->connection->prepare($query);
 
-    if(!$stmt->execute(array(":url" => $url, ":title" => htmlentities($title), ":text" => htmlentities($text), ":poster" => htmlentities($poster_name), ":timestamp" => $timestamp, ":parent" => $parent, ":user_id" => $user_id))){
+    if(!$stmt->execute(array(":url" => $url, ":title" => htmlentities($title), ":text" => htmlentities($text), ":poster" => htmlentities($poster_name), ":timestamp" => $timestamp, ":ip_address" > $ip_address, ":parent" => $parent, ":root" => $root, ":user_id" => $user_id, ":state" => $state))){
       return false;
     }
 
@@ -68,20 +69,25 @@ class BalfComment {
     $output = "";
     $reply = false;
     $reply_class = "";
+    $reply_data = "";
     if($comment['parent'] != -1){
       $reply = true;
+
       $reply_class = " reply";
 
       if($comment['user_id'] != -1){
         $reply_class = " reply is_user";
       }
+      $reply_data = ' data-reply-parent="'.$comment['parent'].'"';
+      $reply_data .= ' data-root-id="'.$comment['root_comment'].'"';
     }
-    $output .= '<div class="balfcomment'.$reply_class.'" id="balfcomment_id_'.$comment['comment_id'].'" data-poster-name="'.$comment['poster_name'].'">';
+
+    $output .= '<div class="balfcomment'.$reply_class.'" id="balfcomment_id_'.$comment['comment_id'].'"'. $reply_data.' data-poster-name="'.$comment['poster_name'].'">';
       $output .= '<div class="wrapper">';
         $output .= '<div class="top">';
           $said = "";//" said";
           if($reply){
-            $said = ' replied to <a href="#balfcomment_id_'.$comment['parent'].'">' . $comments[$comment['parent']]['poster_name'] . '</a>';
+            $said = ' replied to <a href="#balfcomment_id_'.$comment['parent'].'">' . $comments[$comment['root_comment']]['poster_name'] . '</a>';
           }
           $user = "";
           if($comment['user_id'] != -1){
